b9d6057d22804c55399ffdf407220f2ce70a5328
erback <erik.n.back@gmail.com>
2017-06-09 12:17:41 +0200

    Added support for checking directory existence and converting dash to underscore and uppercase

--- pydocker_secrets/__init__.py
+++ pydocker_secrets/__init__.py
@@ -1,17 +1,26 @@
 import os
-def load_secrets_from_dir(directory='/var/run/secrets/'):
+
+
+def load_secrets_from_dir(directory):
     """
     Yields content of files in directory
     """
-    for filename in os.listdir(directory):
-        with open("%s%s" % (directory, filename)) as f:
-            yield filename, f.read().strip()
 
-def get_secrets():
+    for filename in os.listdir(directory):
+        with open(os.path.join(filename, directory)) as f:
+            # Yield the filename on the format "UPPER_UNDERSCORE" instead of "lower-dash"
+            yield filename.replace('-', '_').upper(), f.read().strip()
+
+
+def get_secrets(directory='/var/run/secrets/'):
     """
     Loads secrets into dict
     """
     secrets = {}
-    for filename, secret in load_secrets_from_dir():
+
+    if not os.path.exists(directory):
+        return secrets
+
+    for filename, secret in load_secrets_from_dir(directory):
         secrets[filename] = secret
     return secrets
